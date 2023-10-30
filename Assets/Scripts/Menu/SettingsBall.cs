06e9f80917397efb7253a24bc1480322829fa58f
M1st3r-G <g.haegerbaeumer@gmx.de>
2023-10-30 14:54:01 +0100

    Added Full Functionality to the MainMenu

--- Assets/Scripts/Menu/SettingsBall.cs
+++ Assets/Scripts/Menu/SettingsBall.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour
+public class SettingsBall : MonoBehaviour
 {
+    //OuterComponents
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private Vector3 offset;
     // InnerComponents
     private Rigidbody2D rb;
     // OuterParams
@@ -11,37 +16,16 @@ public class BallController : MonoBehaviour
     // InnerTemps
     private Vector3 startPosition;
 
-    public static int respawnCounter { private set; get; }
-
     // Unity-Awake Methode
     void Awake()
     {
         // Setzen der InnerComponents
         rb = GetComponent<Rigidbody2D>();
         startPosition = transform.position;
-        //Starten der Bewegung
-        rb.velocity = Vector2.up * speed;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void StartMovement ()
     {
-        //Return if not Player
-        if (collision.collider.gameObject.CompareTag("Wall") ||
-            collision.collider.gameObject.CompareTag("Brick")) return;
-
-        // Return if Collission with Players Bottomside
-        if (collision.collider.transform.position.y > transform.position.y) return;
-
-        float diff = transform.position.x - collision.collider.transform.position.x;
-        float percent = diff / PlayerController.platformLength + 0.5f;
-        float rDisplacement = Random.Range(-0.1f, 0.1f);
-        rb.velocity = new Vector2(Mathf.Lerp(-1f, 1f, percent + rDisplacement), 1).normalized * speed;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        transform.position = startPosition;
-        rb.velocity = Vector2.down * rb.velocity.magnitude;
-        respawnCounter++;
+        if(rb.velocity.magnitude < 0.1f) rb.velocity = (target.transform.position + offset - startPosition).normalized * speed;
     }
 }
---
+++ Assets/Scripts/Menu/UIInteractions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIInteractions : MonoBehaviour
+{
+    public void Exit()
+    {
+       Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(1); // Settings
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(2); // Level1
+    }
+}
