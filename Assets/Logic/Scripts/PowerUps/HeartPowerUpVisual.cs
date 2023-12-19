0796badc0510ee1013ae05e1d440f3a784202b8f
MisterG <g.haegerbaeumer@gmx.de>
2023-12-19 14:53:45 +0100

    Restructuring

---
+++ Assets/Logic/Scripts/PowerUps/HeartPowerUpVisual.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+
+public class HeartPowerUpVisual : MonoBehaviour
+{
+    //ComponentReferences
+    //Params
+    //Temps
+    //Publics
+     
+    private void Awake()
+    {
+    
+    }
+}
