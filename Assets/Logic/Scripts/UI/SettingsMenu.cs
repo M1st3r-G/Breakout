6b4be9e04c6b92ba712354dc622c2c6ddf76d721
MisterG <g.haegerbaeumer@gmx.de>
2023-12-19 18:51:18 +0100

    Added A Cool Toggle

--- Assets/Logic/Scripts/UI/SettingsMenu.cs
+++ Assets/Logic/Scripts/UI/SettingsMenu.cs
@@ -1,15 +1,34 @@
-using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     //ComponentReferences
+    [SerializeField] private Toggle PartyToggle;
+    [SerializeField] private Slider SoundSlider;
+    [SerializeField] private GameObject DisplayBricks;
     //Params
     //Temps
+    private Image[] bricks;
     //Publics
      
     private void Awake()
     {
+        DisplayBricks.transform.GetComponentsInChildren<Image>();
+    }
+
+    public void OnPartyToggleChange()
+    {
+        
+    }
+
+    public void ChangeToMainMenu()
+    {
+        // Save in Player Prefs
+    }
     
+    public void OnSoundChange()
+    {
+        
     }
 }
