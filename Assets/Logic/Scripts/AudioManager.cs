4a555be01091627bdebe3aec89fe46eec0a736c3
M1st3r-G <g.haegerbaeumer@gmx.de>
2024-01-04 17:35:50 +0100

    Added Increading Tone Height for Consecutive Hits

--- Assets/Logic/Scripts/AudioManager.cs
+++ Assets/Logic/Scripts/AudioManager.cs
@@ -7,24 +7,25 @@ public class AudioManager : MonoBehaviour
     //ComponentReferences
     [SerializeField] private AudioClip[] clips;
     private AudioSource effectAudioSource;
+    private AudioSource hitAudioSource;
     private AudioSource musicAudioSource;
     //Params
     private float musicVolume;
     private float effectVolume;
     //Temps
+    private int consecCounter;
     //Publics
-    public static readonly int HitDefault = 0;
-    public static readonly int HitWall = 1;
-    public static readonly int HitIndestructible = 2;
-    public static readonly int HitPlatform = 3;
+    public const int HitDefault = 0;
+    public const int HitWall = 1;
+    public const int HitIndestructible = 2;
+    public const int HitPlatform = 3;
 
-    public static readonly int PowerUpCollect = 4;
-    public static readonly int PowerUpHeartCollect = 5;
-    public static readonly int PowerUpRocketStart = 6;
-    public static readonly int PowerUpRocketExplode = 7;
-    
-    public static readonly int GameOver = 8;
-    
+    public const int PowerUpCollect = 4;
+    public const int PowerUpHeartCollect = 5;
+    public const int PowerUpRocketStart = 6;
+    public const int PowerUpRocketExplode = 7;
+
+    public const int GameOver = 8;
     
     private static AudioManager _instance;
     public static AudioManager Instance => _instance;
@@ -38,15 +39,17 @@ public class AudioManager : MonoBehaviour
         }
         _instance = this;
         DontDestroyOnLoad(this);
-        
-        effectAudioSource = GetComponent<AudioSource>();
-        musicAudioSource = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
-
-        musicVolume = PlayerPrefs.GetFloat(SettingsMenu.MusicVolumeKey, 0.75f);
+        AudioSource[] sources = GetComponents<AudioSource>();
+        effectAudioSource = sources[0];
         effectVolume = PlayerPrefs.GetFloat(SettingsMenu.EffectVolumeKey, 0.75f);
-
         effectAudioSource.volume = effectVolume;
+        
+        musicAudioSource = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        musicVolume = PlayerPrefs.GetFloat(SettingsMenu.MusicVolumeKey, 0.75f);
         musicAudioSource.volume = musicVolume;
+
+        hitAudioSource = sources[1];
+        hitAudioSource.volume = effectVolume;
     }
 
     private void OnEnable()
@@ -65,5 +68,14 @@ public class AudioManager : MonoBehaviour
         musicAudioSource.volume = musicVolume;
     }
     
-    public void PlayAudioEffect(int index) => effectAudioSource.PlayOneShot(clips[index]);
+    public void PlayAudioEffect(int index)
+    {
+        if (index == HitPlatform) hitAudioSource.pitch = 1f;
+        if (index == HitDefault)
+        {
+            hitAudioSource.PlayOneShot(clips[index]);
+            hitAudioSource.pitch += 0.1f;
+        }
+        else effectAudioSource.PlayOneShot(clips[index]);
+    }
 }
