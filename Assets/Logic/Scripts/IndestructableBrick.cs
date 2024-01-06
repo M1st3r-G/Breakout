3818891afd752213fa4b62ca1746cc0d1658f7b8
M1st3r-G <g.haegerbaeumer@gmx.de>
2024-01-06 22:46:04 +0100

    Update

--- Assets/Logic/Scripts/IndestructableBrick.cs
+++ Assets/Logic/Scripts/IndestructableBrick.cs
@@ -4,9 +4,7 @@ public class IndestructableBrick: MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        print("hit");
         if (other.gameObject.GetComponent<RocketController>() is null) return;
-        print("By Rocket");
         BrickParticleController.Instance.Indestructible(this);
         Destroy(gameObject);
     }
